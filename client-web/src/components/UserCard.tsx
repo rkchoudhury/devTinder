620da3e636027a1afebec6b74df47d4bb8cc0d54
rkchoudhury <rakeshchoudhury.rocky@gmail.com>
2025-08-25 08:04:29 +0530

    Added User Card

---
+++ client-web/src/components/UserCard.tsx
@@ -0,0 +1,42 @@
+import type { IUser } from "../models/userModel";
+
+export const UserCard = ({ user }: { user: IUser | null }) => {
+  if (!user) return;
+
+  const { firstName, lastName, about, photoUrl, gender, age, skills } = user;
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <figure>
+          <img src={photoUrl} alt="User Photo" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{firstName + " " + lastName}</h2>
+          {age && gender && (
+            <p>
+              {age} {gender}
+            </p>
+          )}
+          <p>{about}</p>
+          {skills.length > 0 && (
+            <div className="flex">
+              <div className="mr-2">Skills:</div>
+              <div>
+                {skills?.map((name) => (
+                  <div className="badge badge-md badge-outline badge-primary mr-2 mb-2">
+                    {name}
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+          <div className="card-actions justify-center mt-2">
+            <button className="btn btn-primary mr-2">Ignore</button>
+            <button className="btn btn-secondary">Interested</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
