9f5395c8c700937a28ff8bbed90bd851068374b6
rkchoudhury <rakeshchoudhury.rocky@gmail.com>
2025-08-30 12:22:40 +0530

    Added Feed API and saved the data onto the store

--- client-web/src/screens/feed/Feed.tsx
+++ client-web/src/screens/feed/Feed.tsx
@@ -1,13 +1,44 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { UserCard } from "../../components/UserCard";
 import type { RootState } from "../../redux/store";
-import type { IUser } from "../../models/userModel";
+import { useEffect } from "react";
+import { getFeed } from "../../services/userService";
+import type { AxiosError } from "axios";
+import { showAlert } from "../../redux/slices/alertSlice";
+import { addFeed } from "../../redux/slices/feedSlice";
 
 const Feed = () => {
-  const user: IUser | null = useSelector((state: RootState) => state.user);
+  const dispatch = useDispatch();
+  const { list } = useSelector((state: RootState) => state.feed);
+
+  useEffect(() => {
+    const fetchFeed = async () => {
+      try {
+        const response = await getFeed();
+        dispatch(addFeed(response));
+      } catch (error) {
+        const axiosError = error as AxiosError;
+        dispatch(
+          showAlert({
+            showAlert: true,
+            message: axiosError?.message,
+            duration: 5000,
+          })
+        );
+      }
+    };
+    fetchFeed();
+  }, [dispatch]);
+
+  if (!list) return null;
+
+  if (list?.length === 0) {
+    return <h1>No Connections</h1>;
+  }
+
   return (
     <div>
-      <UserCard user={user} />
+      <UserCard user={list[0]} showButton />
     </div>
   );
 };
---
+++ client-web/src/services/userService.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosError } from "axios";
+
+import { BASE_URL } from "../utils/apiConfig";
+
+const getFeed = async (page: number = 1, limit: number = 10) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/user/feed?page=${page}&limit=${limit}`,
+      {
+        withCredentials: true,
+      }
+    );
+    return response?.data;
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    throw axiosError?.response?.data;
+  }
+};
+
+export { getFeed };
