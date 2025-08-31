9cfe19bf2fcbb0de047644fc635570fc996635fa
rkchoudhury <rakeshchoudhury.rocky@gmail.com>
2025-08-31 21:56:06 +0530

    Code cleanup

---
+++ client-mob/app/index.tsx
@@ -0,0 +1,16 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "green" }}>
+      <Text style={{ color: "black" }}>Heyy</Text>
+      <Text style={{ color: "red" }}>Heyy</Text>
+      <Text style={{ color: "red" }}>Heyy</Text>
+      <Text style={{ color: "red" }}>Heyy</Text>
+      <Text style={{ color: "red" }}>Heyy</Text>
+      <Text style={{ color: "red" }}>Heyy</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
