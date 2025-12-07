d45a1e2f41e9beac396ca96662485e239ee42cbe
rkchoudhury <rakeshchoudhury.rocky@gmail.com>
2025-12-07 21:52:44 +0530

    Saved Order details in DB

---
+++ server/src/models/payment.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+
+const { Schema, model } = mongoose;
+
+const paymentSchema = new Schema(
+    {
+        amount: {
+            type: Number,
+            required: true,
+        },
+        currency: {
+            type: String,
+            required: true,
+        },
+        orderId: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        status: {
+            type: String,
+            required: true,
+        },
+        receipt: {
+            type: String,
+            required: true,
+        },
+        notes: {
+            firstName: {
+                type: String,
+            },
+            lastName: {
+                type: String,
+            },
+            emailId: {
+                type: String,
+            },
+            membershipType: {
+                type: String,
+                enum: {
+                    values: ["gold", "silver"],
+                    message: '{VALUE} is incorrect membership type.'
+                },
+                required: true,
+            }
+        },
+        paymentId: {
+            type: String,
+        },
+        userId: {
+            type: mongoose.Types.ObjectId,
+            ref: "User",
+            required: true,
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Payment = model("Payment", paymentSchema);
+
+module.exports = Payment;
--- server/src/routes/payment.js
+++ server/src/routes/payment.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 
 const paymentRouter = express.Router();
+const razorpayInstnace = require("../utils/paymentUtils/razorpay");
+const Payment = require("../models/payment");
 
 /**
  * 1. Create the Order on RozarPay
@@ -11,8 +13,34 @@ paymentRouter.post("/payment/create", userAuth, async (req, res) => {
     try {
         const user = req.user;
 
-        // Sending custom data as a json from the server
-        res.status(200).json({ data: user, status: "success" });
+        // Create an order on RazorPay
+        const order = await razorpayInstnace.orders.create({
+            amount: 50000, // Rs: 500 - Lower currency will be passed
+            currency: "INR",
+            receipt: "receipt#1",
+            notes: {
+                firstName: user.firstName,
+                lastName: user.lastName,
+                emailId: user.emailId,
+                membershipType: "silver",
+            },
+        });
+
+        // Save the order data on DB
+        const { id, amount, currency, status, receipt, notes } = order;
+        const payment = new Payment({
+            userId: user._id,
+            orderId: id,
+            status,
+            amount,
+            currency,
+            receipt,
+            notes
+        });
+        const savedPayment = await payment.save();
+
+        // Sending order data as a json to frontend
+        res.status(200).json({ data: savedPayment, status: "success" });
     } catch (error) {
         res.status(400).send("Error: " + error.message);
     }
