25b8f40e680b6350830fef2dfbe37d7edf2dfbaf
Rakesh Choudhury <Rakesh.Choudhury@gds.ey.com>
2025-12-17 22:31:00 +0530

    Added chat contents

--- client-web/src/screens/chat/Chat.tsx
+++ client-web/src/screens/chat/Chat.tsx
@@ -10,9 +10,34 @@ export const Chat: React.FC = () => {
   const { userId } = (location.state as ChatLocationState) || {};
 
   return (
-    <div>
-      <h1>Chat Screen</h1>
-      <h1>userId: {userId}</h1>
+    <div className='flex flex-col items-center justify-center'>
+      <div className='flex flex-col items-center justify-center border-2 rounded-lg shadow-md w-1/2 mt-10'>
+        <h1 className='border-b-2 py-4 mb-4 w-full text-center'>Chat</h1>
+        <div className='h-[60vh] w-full px-2'>
+          <div className="chat chat-start">
+            <div className="chat-header">
+              Obi-Wan Kenobi
+              <time className="text-xs opacity-50">2 hours ago</time>
+            </div>
+            <div className="chat-bubble chat-bubble-info">You were the Chosen One!</div>
+          </div>
+
+           <div className="chat chat-end">
+            <div className="chat-header">
+              Obi-Wan Kenobi
+              <time className="text-xs opacity-50">2 hours ago</time>
+            </div>
+            <div className="chat-bubble chat-bubble-accent">
+              That's never been done in the history of the Jedi.
+            </div>
+          </div>
+          
+        </div>
+        <div className='border-t-2 py-4 px-4 w-full flex justify-end'>
+          <input type="text" placeholder="Type a message..." className="input input-md" />
+          <button className="btn btn-primary ml-4">Send</button>
+        </div>
+      </div>
     </div>
   );
 };
